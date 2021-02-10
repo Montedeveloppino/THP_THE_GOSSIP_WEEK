846ca2a83062383a179ca206eb5f1979c6dd892c
Youssef <annabiyoussef1986@gmail.com>
2021-02-11 00:42:16 +0100

    ajout auteurs

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
-User.destroy_all
-Gossip.destroy_all
+user = User.create(first_name: "Chris", last_name:"brown", bio: "le meilleur", email: "fatener@thp.fr", age: 34, city: "Paris")
 
 10.times do
 	user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, bio: Faker::Lorem.paragraph_by_chars(140, false), email: Faker::Internet.email, age: rand(18..80), city: Faker::Address.city)
@@ -16,3 +15,4 @@ end
 10.times do
   gossip = Gossip.create!(title: Faker::Lorem.characters(number: 4), content: Faker::Movie.quote, user_id: rand(User.all.first.id..User.all.last.id))
 end
+
