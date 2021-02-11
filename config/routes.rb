0a0c8e7e80524bb175fc1a0878065ebf8c1af02c
Yann Rannou <yann.rannou777@gmail.com>
2021-02-11 21:42:01 +0100

    Boostrap 001

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get'/home', to: 'home#home'
+  get'/index', to: 'gossip#index'
+  get 'static_pages/contact'
+  get 'static_pages/team'
   get "signup", to: "users#new", as: "signup"
   get "login", to: "sessions#new", as: "login"
   get "logout", to: "sessions#destroy", as: "logout"
@@ -6,5 +10,5 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
     resources :gossip do
       resources :comments
-    end   
+    end
 end
