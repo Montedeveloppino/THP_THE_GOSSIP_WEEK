040f95bcd3d964b1733b80bbf400344f826b9b3f
Theo CAZENAVE <theocazenavecoupet@gmail.com>
2021-02-11 09:47:24 +0100

    user add password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
+    @user.save
   end
 
   def update
@@ -31,10 +31,12 @@ class UsersController < ApplicationController
   private
 
   def set_user
-      @user = User.find(params[:id])
-    end
+    @user = User.find(params[:id])
+  end
 
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :bio, :email, :age, :city, :password_digest)
-    end
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :bio, :email, :age, :city, :password_digest)
+  end
+
 end
+ 
